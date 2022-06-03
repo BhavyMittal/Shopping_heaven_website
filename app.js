4cfe5013aa67b94cc3c625a56ded8f877b476874
BhavyMittal <98758245+BhavyMittal@users.noreply.github.com>
2022-06-03 17:32:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+function mouse()
+{
+    window.location="mouse.html";
+}
+function mobile()
+{
+    window.location ="mobile.html";
+}
+function tablet()
+{
+    window.location ="tablet.html";
+}
+function earbuds()
+{
+    window.location ="earbuds.html";
+}
+function smartwatch()
+{
+    window.location = "smartwatch.html";
+}
+function earphone()
+{
+    window.location ="earphones.html";
+}
